00a25de7a6bd8b4ef90a67fce47e07e5086d9e15
shivkumarprajapat <shivkumarprajapat28@gmail.com>
2021-10-22 13:35:05 +0530

    Categories is Complete

--- screens/Home.js
+++ screens/Home.js
@@ -2,16 +2,18 @@ import React from 'react'
 import HeaderTab from '../components/HeaderTab'
 import { SafeAreaView, View } from 'react-native'
 import SearchBar from '../components/SearchBar'
+import Categories from '../components/Categories'
 export default function Home() {
     return (
 
         // Not Working SafeAreaView
-        
+
         <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
             <View style={{ backgroundColor: "white", padding: 15, paddingTop: 30 }}>
                 <HeaderTab />
                 <SearchBar />
             </View>
+            <Categories/>
         </SafeAreaView>
     )
 }
